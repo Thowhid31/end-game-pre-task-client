4e63f04f8b2a7d55da807279f7dd9201ad47d8bb
Thowhid31 <thowhidantor16@gmail.com>
2022-06-30 23:34:24 +0600

    added calender

--- src/Pages/Calender.js
+++ src/Pages/Calender.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from '../assets/list-green-stationery.jpg';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+
 
 const Calender = () => {
+
+    const [date, setDate] = useState(new Date)
+
     return (
         <div class="hero min-h-screen bg-base-200">
             <div class="hero-content flex-col lg:flex-row">
                 <img className='w-96 flex-1 rounded-md' src={Todo} alt="" />
                 <div className='flex-1'>
-                    
+                <DayPicker 
+                
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                />
+                <p className='font-bold'>Your Selected Date: {format(date, 'PP')}</p>
                 </div>
             </div>
+            
         </div>
     );
 };
